0cdb56d3a1fbfb46745494e86702ac957761cc36
piyushk <piyushk@gmail.com>
2013-01-04 12:45:26 +0530

    bug fixes + now publishing the footprint polygon + appropriate configuration changes

--- segbot_sensors/include/segbot_sensors/footprint_filter.h
+++ segbot_sensors/include/segbot_sensors/footprint_filter.h
@@ -4,6 +4,7 @@
 #include <tf/transform_listener.h>
 #include <filters/filter_base.h>
 #include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/PolygonStamped.h>
 #include <XmlRpcException.h>
 
 namespace segbot_sensors
@@ -15,6 +16,7 @@ namespace segbot_sensors
       std::string footprint_frame_;
       bool get_transformation_;
       boost::shared_ptr<tf::TransformListener> listener_;
+      ros::Publisher footprint_publisher_;
 
       bool configure() {
 
@@ -65,6 +67,10 @@ namespace segbot_sensors
           return false;
         }
 
+        // Get ready to publish footprint
+        ros::NodeHandle n("~");
+        footprint_publisher_ = n.advertise<geometry_msgs::PolygonStamped>("footprint", 1);
+
         return true;
       }
 
@@ -76,29 +82,41 @@ namespace segbot_sensors
         if (get_transformation_) {
           try {
             tf::StampedTransform transform;
-            listener_->waitForTransform(footprint_frame_, input_scan.header.frame_id, ros::Time(0), ros::Duration(10.0) );
-            listener_->lookupTransform(footprint_frame_, input_scan.header.frame_id, ros::Time(0), transform);
+            listener_->waitForTransform(input_scan.header.frame_id, footprint_frame_, ros::Time(0), ros::Duration(10.0) );
+            listener_->lookupTransform(input_scan.header.frame_id, footprint_frame_, ros::Time(0), transform);
             for (uint32_t i = 0; i < footprint_.size(); ++i) {
               footprint_[i] = transform * footprint_[i]; 
             }
-            
+            ROS_INFO("Transformation from %s to %s received. Transforming footprint", footprint_frame_.c_str(), input_scan.header.frame_id.c_str());
           } catch (tf::TransformException ex) {
             ROS_ERROR("Error getting transformation. The filtered scan is probably incorrect: %s",ex.what());
           }
           get_transformation_ = false;
         }
 
+        // Publish polygon corresponding to footprint - visualization
+        geometry_msgs::PolygonStamped polygon_msg;
+        polygon_msg.header.frame_id = input_scan.header.frame_id;
+        polygon_msg.header.stamp = ros::Time::now();
+        polygon_msg.polygon.points.resize(footprint_.size());
+        for (uint32_t i = 0; i < footprint_.size(); ++i) {
+          polygon_msg.polygon.points[i].x = footprint_[i].x();
+          polygon_msg.polygon.points[i].y = footprint_[i].y();
+          polygon_msg.polygon.points[i].z = 0;
+        }
+        footprint_publisher_.publish(polygon_msg);
+
         filtered_scan.ranges.resize(input_scan.ranges.size());
         double current_angle = input_scan.angle_min;
 
-        for(unsigned int i = 0; i < input_scan.ranges.size(); ++i){
+        for(unsigned int count = 0; count < input_scan.ranges.size(); ++count){
           current_angle += input_scan.angle_increment;
-          if (input_scan.range_min <= input_scan.ranges[i] && 
-              input_scan.ranges[i] <= input_scan.range_max) {
+          if (input_scan.range_min <= input_scan.ranges[count] && 
+              input_scan.ranges[count] <= input_scan.range_max) {
             // Valid measurement, check if it lies in the footprint
             // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
-            float testx = cosf(current_angle) * input_scan.ranges[i];
-            float testy = sinf(current_angle) * input_scan.ranges[i];
+            float testx = cosf(current_angle) * input_scan.ranges[count];
+            float testy = sinf(current_angle) * input_scan.ranges[count];
             int i, j, c = 0;
             int nvert = footprint_.size(); 
             for (i = 0, j = nvert-1; i < nvert; j = i++) {
@@ -107,7 +125,7 @@ namespace segbot_sensors
                  c = !c;
             }
             // in accordance with REP 117
-            filtered_scan.ranges[i] = (c) ? input_scan.ranges[i] : std::numeric_limits<float>::quiet_NaN();
+            filtered_scan.ranges[count] = (!c) ? input_scan.ranges[count] : std::numeric_limits<float>::quiet_NaN();
           }
         }
 
