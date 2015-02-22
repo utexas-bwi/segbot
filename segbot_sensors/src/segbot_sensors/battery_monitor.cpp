f3ab23fd5fc04a7c278cc425b451554b14959460
Maxwell Svetlik <maxsvetlik@utexas.edu>
2015-02-22 15:33:28 -0600

    got dummy node publishing to /diagnostics. Recieved by diagnostic system and aggregator

--- segbot_sensors/src/segbot_sensors/battery_monitor.cpp
+++ segbot_sensors/src/segbot_sensors/battery_monitor.cpp
@@ -1,35 +1,40 @@
 #include "ros/ros.h"
+#include "ros/time.h"
 #include <vector>
+#include "std_msgs/Header.h"
 #include "std_msgs/Float32.h"
 #include "diagnostic_msgs/DiagnosticStatus.h"
+#include "diagnostic_msgs/DiagnosticArray.h"
 #include "diagnostic_msgs/KeyValue.h"
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "battery_life_estimator");
+  ros::init(argc, argv, "battery_estimator");
   ros::NodeHandle n;
 
-  ros::Publisher battery_life_pub = n.advertise<diagnostic_msgs::DiagnosticStatus>("/battery_estimator", 10);
+  ros::Publisher battery_life_pub = n.advertise<diagnostic_msgs::DiagnosticArray>("diagnostics", 10);
 
   ros::Rate loop_rate(10);
   diagnostic_msgs::DiagnosticStatus status;
+  diagnostic_msgs::DiagnosticArray diagAr;
   status.level = 0;
   status.name = "battery_estimator";
   status.hardware_id = "005";
-  int count = 0;
   diagnostic_msgs::KeyValue val;
-  std::vector<diagnostic_msgs::KeyValue> kvals;
+  val.key = "bat_mon";
+  val.value = "183.0";
   val.key = "battery";
   while (ros::ok())
   {
+    diagAr.header.stamp = ros::Time::now();
+    diagAr.header.frame_id = 1;
     float voltage = 12.3;
     float estimated_life = 183.0;
     if(voltage > 10.5)
 	status.message = "Battery in good health";
-    val.value = estimated_life;
-    kvals.push_back(val);
-    status.values = kvals;
 
-    battery_life_pub.publish(status);
+    status.values.push_back( val);
+    diagAr.status.push_back(status);
+    battery_life_pub.publish(diagAr);
 
     ros::spinOnce();
 
