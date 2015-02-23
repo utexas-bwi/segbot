ce6642057c1a7aab5b89ca7875a9f32428fea941
Maxwell Svetlik <maxsvetlik@utexas.edu>
2015-02-22 23:53:32 -0600

    added voltage error tiering

--- segbot_sensors/src/segbot_sensors/battery_monitor.cpp
+++ segbot_sensors/src/segbot_sensors/battery_monitor.cpp
@@ -13,31 +13,34 @@ int main(int argc, char **argv)
 
   ros::Publisher battery_life_pub = n.advertise<diagnostic_msgs::DiagnosticArray>("diagnostics", 10);
 
-  ros::Rate loop_rate(10);
+  ros::Rate loop_rate(1);
   diagnostic_msgs::DiagnosticStatus status;
   diagnostic_msgs::DiagnosticArray diagAr;
-  status.level = 0;
-  status.name = "battery_estimator";
-  status.hardware_id = "005";
   diagnostic_msgs::KeyValue val;
-  val.key = "bat_mon";
+  status.hardware_id = "005";
+  val.key = "battery_value";
   val.value = "183.0";
-  val.key = "battery";
+
   while (ros::ok())
   {
     diagAr.header.stamp = ros::Time::now();
     diagAr.header.frame_id = 1;
     float voltage = 12.3;
     float estimated_life = 183.0;
-    if(voltage > 10.5)
+    if(voltage > 11.0){
 	status.message = "Battery in good health";
-
-    status.values.push_back( val);
+	status.level = 0; // 0 = OK
+    }else if(voltage > 10 && voltage < 11){
+	status.message = "Battery low, return to lab.";
+	status.level = 1; // WARN
+    }else{
+	status.message = "Battery CRITICALLY low, or voltmeter data is inaccurate (or missing). Ensure the volt sensor is connected properly and its publisher relaying data.";
+	status.level = 0; // CRITICAL
+    }
+    status.values.push_back(val);
     diagAr.status.push_back(status);
     battery_life_pub.publish(diagAr);
-
     ros::spinOnce();
-
     loop_rate.sleep();
   }
 
