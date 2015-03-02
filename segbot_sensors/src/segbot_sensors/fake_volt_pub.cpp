81eac840540253af00fa08824a06ad2172092d30
Maxwell Svetlik <maxsvetlik@utexas.edu>
2015-03-01 23:10:09 -0600

    fixed tab & spacing inconsistency

--- segbot_sensors/src/segbot_sensors/fake_volt_pub.cpp
+++ segbot_sensors/src/segbot_sensors/fake_volt_pub.cpp
@@ -5,11 +5,8 @@
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "talker");
-
   ros::NodeHandle n;
-
   ros::Publisher chatter_pub = n.advertise<smart_battery_msgs::SmartBatteryStatus>("smart_battery_msgs/SmartBatteryStatus", 1);
-
   ros::Rate loop_rate(10);
 
   int count = 0;
@@ -21,7 +18,5 @@ int main(int argc, char **argv){
 
     loop_rate.sleep();
   }
-
-
   return 0;
 }
