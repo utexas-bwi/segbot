5c49b45b611701296a01a4871f9c6eac74da73c4
Piyush Khandelwal <piyushk@gmail.com>
2016-04-25 15:00:59 -0500

    finished + tested compilation of robot teleportation script.

--- segbot_simulation_apps/src/nodes/robot_teleporter.cpp
+++ segbot_simulation_apps/src/nodes/robot_teleporter.cpp
@@ -1,61 +1,54 @@
+#include <boost/shared_ptr.hpp>
+#include <stdexcept>
+
+#include <gazebo_msgs/GetModelState.h>
+#include <gazebo_msgs/SetModelState.h>
 #include <ros/ros.h>
 
 #include <bwi_msgs/RobotTeleporterInterface.h>
+#include <segbot_simulation_apps/common.h>
 
-bool execute(bwi_msgs::RobotTeleporterInterface::Request  &req,
+std::string robot_model_name_;
+boost::shared_ptr<ros::ServiceClient> get_gazebo_model_client_;
+boost::shared_ptr<ros::ServiceClient> set_gazebo_model_client_;
+
+bool execute(bwi_msgs::RobotTeleporterInterface::Request &req,
              bwi_msgs::RobotTeleporterInterface::Response &res) {
 
-  res.status = "";
-  if (!req.room.empty() && !req.door.empty()) {
-
-
-  if (req.all_doors) {
-    if (req.open) {
-      gh_->openAllDoors();
-    } else {
-      gh_->closeAllDoors();
-    }
-    res.success = true;
-  } else {
-    if (req.open) {
-      res.success = gh_->openDoor(req.door);
-    } else {
-      res.success = gh_->closeDoor(req.door);
-    }
-    if (!res.success) {
-      res.status = "Unable to resolved " + req.door + "!";
-    }
-  } else {
-    res.success = False;
-    res.status = "Malformed request. Either room(" + req.room + ") or door(" + req.door + ") is empty.";
-  }
-
+  res.success = segbot_simulation_apps::teleportEntity(robot_model_name_,
+                                                       req.pose,
+                                                       *get_gazebo_model_client_,
+                                                       *set_gazebo_model_client_);
   return true;
 }
 
 int main(int argc, char *argv[]) {
 
-    get_gazebo_model_client_ =
-      nh.serviceClient<gazebo_msgs::GetModelState>(
-          "/gazebo/get_model_state");
-    bool gazebo_available = 
-      get_gazebo_model_client_.waitForExistence(ros::Duration(30));
-
-    if (!gazebo_available) {
-      ROS_FATAL_STREAM("ClingoGazeboHandler: Gazebo is NOT AVAILABLE");
-      throw 
-        std::runtime_error("ClingoGazeboHandler: Gazebo is NOT AVAILABLE");
-    }
-
-    set_gazebo_model_client_ =
-      nh.serviceClient<gazebo_msgs::SetModelState>(
-          "/gazebo/set_model_state");
-    set_gazebo_model_client_.waitForExistence();
-
   ros::init(argc, argv, "gazebo_robot_teleporter");
   ros::NodeHandle nh;
+  ros::NodeHandle private_nh("~");
+
+  if (!private_nh.getParam("robotid", robot_model_name_)) {
+    ROS_FATAL_STREAM("GazeboRobotTeleporter: Parameter ~robotid needs to be set to indicate the robot entity name " <<
+                     "inside Gazebo.");
+    return -1;
+  }
+
+  get_gazebo_model_client_.reset(new ros::ServiceClient);
+  *get_gazebo_model_client_ = nh.serviceClient<gazebo_msgs::GetModelState>( "/gazebo/get_model_state");
+  bool gazebo_available = get_gazebo_model_client_->waitForExistence(ros::Duration(30));
+
+  if (!gazebo_available) {
+    ROS_FATAL_STREAM("GazeboRobotTeleporter: Gazebo is NOT AVAILABLE");
+    return -1;
+  }
+
+  set_gazebo_model_client_.reset(new ros::ServiceClient);
+  *set_gazebo_model_client_ = nh.serviceClient<gazebo_msgs::SetModelState>( "/gazebo/set_model_state");
+  set_gazebo_model_client_->waitForExistence();
 
   ros::ServiceServer service = nh.advertiseService("teleport_robot", execute);
   ros::spin();
+
   return 0;
 }
