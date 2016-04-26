d274722699bfe60a6e329155260f589c0f277a8d
Piyush Khandelwal <piyushk@gmail.com>
2016-04-26 17:22:28 -0500

    added some code to remove redundant ros timers from the timeout queue. fixed incorrect footprint clearing.

--- segbot_simulation_apps/src/nodes/door_handler.cpp
+++ segbot_simulation_apps/src/nodes/door_handler.cpp
@@ -5,20 +5,17 @@
 
 boost::shared_ptr<segbot_simulation_apps::DoorHandler> gh_;
 std::vector<boost::shared_ptr<ros::Timer> > timeout_queue_;
-std::vector<bwi_msgs::DoorHandlerInterface::Request> timeout_requests_;
 
-void executeSingleTimeout(const ros::TimerEvent&) {
-  bwi_msgs::DoorHandlerInterface::Request req = *(timeout_requests_.begin());
-
+void executeSingleTimeout(const ros::TimerEvent&, 
+                          bwi_msgs::DoorHandlerInterface::Request req,
+                          boost::shared_ptr<ros::Timer> timer) {
   if (req.all_doors) {
     gh_->closeAllDoors();
   } else {
     /* bool success = */ gh_->closeDoor(req.door);
     // TODO do something with the success result? This was an automated request.
   }
-
-  timeout_queue_.erase(timeout_queue_.begin());
-  timeout_requests_.erase(timeout_requests_.begin());
+  timeout_queue_.erase(std::remove(timeout_queue_.begin(), timeout_queue_.end(), timer), timeout_queue_.end());
 }
 
 bool execute(bwi_msgs::DoorHandlerInterface::Request  &req,
@@ -46,9 +43,8 @@ bool execute(bwi_msgs::DoorHandlerInterface::Request  &req,
   if (req.open && req.open_timeout > 0) {
     boost::shared_ptr<ros::Timer> timer(new ros::Timer);
     ros::NodeHandle nh;
-    *timer = nh.createTimer(ros::Duration(req.open_timeout), executeSingleTimeout);
+    *timer = nh.createTimer(ros::Duration(req.open_timeout), boost::bind(executeSingleTimeout, _1, req, timer), true);
     timeout_queue_.push_back(timer);
-    timeout_requests_.push_back(req);
   }
   return true;
 }
