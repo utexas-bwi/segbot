5b5b0e71552dcc4d70b9962b3df37071565332ce
Piyush Khandelwal <piyushk@gmail.com>
2016-04-26 14:31:58 -0500

    updated simulated calls that open the doors to automatically close those doors.

--- segbot_simulation_apps/src/nodes/door_handler.cpp
+++ segbot_simulation_apps/src/nodes/door_handler.cpp
@@ -4,6 +4,22 @@
 #include <segbot_simulation_apps/door_handler.h>
 
 boost::shared_ptr<segbot_simulation_apps::DoorHandler> gh_;
+std::vector<boost::shared_ptr<ros::Timer> > timeout_queue_;
+std::vector<bwi_msgs::DoorHandlerInterface::Request> timeout_requests_;
+
+void executeSingleTimeout(const ros::TimerEvent&) {
+  bwi_msgs::DoorHandlerInterface::Request req = *(timeout_requests_.begin());
+
+  if (req.all_doors) {
+    gh_->closeAllDoors();
+  } else {
+    /* bool success = */ gh_->closeDoor(req.door);
+    // TODO do something with the success result? This was an automated request.
+  }
+
+  timeout_queue_.erase(timeout_queue_.begin());
+  timeout_requests_.erase(timeout_requests_.begin());
+}
 
 bool execute(bwi_msgs::DoorHandlerInterface::Request  &req,
              bwi_msgs::DoorHandlerInterface::Response &res) {
@@ -27,6 +43,13 @@ bool execute(bwi_msgs::DoorHandlerInterface::Request  &req,
     }
   }
 
+  if (req.open && req.open_timeout > 0) {
+    boost::shared_ptr<ros::Timer> timer(new ros::Timer);
+    ros::NodeHandle nh;
+    *timer = nh.createTimer(ros::Duration(req.open_timeout), executeSingleTimeout);
+    timeout_queue_.push_back(timer);
+    timeout_requests_.push_back(req);
+  }
   return true;
 }
 
